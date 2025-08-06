d2a5f94fa4468524a815bd685169a939baeb113b
Jiae Park <wldo7237@gmail.com>
2025-08-06 14:10:35 -0700

    ex

--- app.js
+++ app.js
@@ -1,3 +1,32 @@
 $(function() {
-  // YOUR CODE HERE
+  $('#toggleButton').on('click', function() {
+    $('#myParagraph').toggle()
+  })
+
+  $('#colorButton').on('click', function() {
+    $('#colorDiv').css('background-color', 'yellow')
+  })
+
+  $('#addClassButton').on('click', function() {
+    const newBox = $('<div class="newClass"></div>');
+    $('#classDiv').append(newBox);
+  })
+  $('#removeClassButton').on('click', function() {
+    $('#classDiv .newClass').last().remove()
+  })
+
+  $('#fadeInButton').on('click', function() {
+    $('#fadeDiv').fadeIn()
+  })
+  $('#fadeOutButton').on('click', function() {
+    $('#fadeDiv').fadeOut()
+  })
+
+  $("#slideUpButton").on('click', function() {
+    $("#slideDiv").slideUp()
+  })
+  $("#slideDownButton").on('click', function() {
+    $("#slideDiv").slideDown()
+  })
 })
+
